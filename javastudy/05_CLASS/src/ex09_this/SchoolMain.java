0cb83d38e56e2e55f47e94ef0de240c32be1c02a
lji170 <wjddlr3865@naver.com>
2022-07-29 17:26:29 +0900

    2022-07-29

--- javastudy/05_CLASS/src/ex09_this/SchoolMain.java
+++ javastudy/05_CLASS/src/ex09_this/SchoolMain.java
@@ -7,14 +7,15 @@ public class SchoolMain {
 		Student student1 = new Student();
 		student1.setStuNo("11025");
 		student1.setName("전지현");
-		
+
 		Student student2 = new Student("11026", "정우성");
 		
 		School school = new School(2);
-		school.printStudents();
 		school.addStudent(student1);
 		school.addStudent(student2);
 		school.addStudent(student1);
-	
+		school.printStudents();
+		
 	}
+
 }

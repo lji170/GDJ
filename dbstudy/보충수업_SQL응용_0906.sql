a6f109a0d756e821241a934a7f316293891cba04
lji170 <wjddlr3865@naver.com>
2022-09-15 17:31:43 +0900

    2022-09-15

---
+++ dbstudy/보충수업_SQL응용_0906.sql
@@ -0,0 +1,7 @@
+
+Function GET_STUDENT이(가) 컴파일되었습니다.
+
+LINE/COL  ERROR
+--------- -------------------------------------------------------------
+14/1      PLS-00103: Encountered the symbol "SELECT" 
+오류: 컴파일러 로그를 확인하십시오.
